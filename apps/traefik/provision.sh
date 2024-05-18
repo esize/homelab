6cd77ad6cca362502e6ff004a28bea2b53bd959b
Evan Sizemore <evan@wool.homes>
2024-05-18 00:08:03 -0400

    fixed sed error

--- apps/traefik/provision.sh
+++ apps/traefik/provision.sh
@@ -7,6 +7,6 @@ chmod 600 /certificates/acme.json
 
 # copy configuration template and replace variables
 cp /traefik.template.yml /etc/traefik/traefik.yml
-sed -i "s/\${DOMAIN_NAME}/$DOMAIN_NAME/" /etc/traefik/traefik.yml
-sed -i "s/\${ADMIN_EMAIL}/$ADMIN_EMAIL/" /etc/traefik/traefik.yml
-sed -i "s/\${LETS_ENCRYPT_ENV}/$LETS_ENCRYPT_ENV/" /etc/traefik/traefik.yml
+sed -i "s|\${DOMAIN_NAME}|$(printf '%s\n' "$DOMAIN_NAME" | sed 's/[&/\]/\\&/g')|" /etc/traefik/traefik.yml
+sed -i "s|\${ADMIN_EMAIL}|$(printf '%s\n' "$ADMIN_EMAIL" | sed 's/[&/\]/\\&/g')|" /etc/traefik/traefik.yml
+sed -i "s|\${LETS_ENCRYPT_ENV}|$(printf '%s\n' "$LETS_ENCRYPT_ENV" | sed 's/[&/\]/\\&/g')|" /etc/traefik/traefik.yml

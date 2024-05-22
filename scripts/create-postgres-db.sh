2a138d1b1e1703db5076e84831429e54d6944202
Evan Sizemore <evan@wool.homes>
2024-05-22 16:11:45 -0400

    fixed db creation script

--- scripts/create-postgres-db.sh
+++ scripts/create-postgres-db.sh
@@ -54,7 +54,7 @@ function create_user_and_database() {
 				echo "User '${database}' does not exist. Creating user ${database}..."
 
 				docker exec homelab-postgres psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" <<-EOSQL
-					CREATE USER $database WITH PASSWORD '$POSTGRES_PASSWORD';
+					CREATE USER $database WITH PASSWORD '$database';
 				EOSQL
                 echo -e "Done!\n"
 		fi
